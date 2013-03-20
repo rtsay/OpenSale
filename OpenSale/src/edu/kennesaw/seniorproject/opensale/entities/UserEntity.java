617cd4fda9d775cf17d98cbba715826ca3a1340d
michael <mcbeckler@gmail.com>
2013-03-20 14:42:22 -0400

    Updated UserEntity to comply with the back-end.

---
+++ OpenSale/src/edu/kennesaw/seniorproject/opensale/entities/UserEntity.java
@@ -0,0 +1,88 @@
+package edu.kennesaw.seniorproject.opensale.entities;
+
+// TODO: Add actual user fields, methods.
+
+import edu.common.UserObjects.EUserTypes;
+import edu.common.UserObjects.User;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * User entity.
+ * @author spencer
+ */
+@Entity
+public class UserEntity extends User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    @Override
+    public EUserTypes getUserType() {
+        return this.userType;
+    }
+
+    @Override
+    public void setUserType(EUserTypes userType) {
+        this.userType = userType;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userName != null ? userName.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserEntity)) {
+            return false;
+        }
+        UserEntity other = (UserEntity) object;
+        if ((this.userName == null && other.userName != null) || (this.userName != null && !this.userName.equals(other.userName))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.kennesaw.seniorproject.opensale.model.User[ id=" + userName + " ]";
+    }
+}

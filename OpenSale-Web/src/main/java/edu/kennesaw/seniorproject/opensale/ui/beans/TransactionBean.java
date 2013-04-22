f8ca8ec1bdf752f09d51e5d9124667d54eb57860
spencerwi <spencerwi@gmail.com>
2013-04-22 09:45:10 -0400

    Added discount support for dollar-amount discounts. Little in-page tweaks. (p:inputMask is buggy, it seems).

--- OpenSale-Web/src/main/java/edu/kennesaw/seniorproject/opensale/ui/beans/TransactionBean.java
+++ OpenSale-Web/src/main/java/edu/kennesaw/seniorproject/opensale/ui/beans/TransactionBean.java
@@ -1,9 +1,10 @@
 package edu.kennesaw.seniorproject.opensale.ui.beans;
 
+import edu.common.Exceptions.NoCurrentSessionException;
+import edu.common.UserObjects.EUserTypes;
 import edu.kennesaw.seniorproject.opensale.entities.ItemEntity;
+import edu.kennesaw.seniorproject.opensale.entities.ProductEntity;
 import edu.kennesaw.seniorproject.opensale.ui.utilities.InPageMessage;
-import edu.opensale.Payment.LegalTender;
-import edu.opensale.PaymentTypes.PaymentFactory;
 import edu.product.ProductObjects.Product;
 import edu.transaction.TransactionObjects.Item;
 import edu.transaction.TransactionObjects.Transaction;
@@ -42,8 +43,15 @@ public class TransactionBean {
     @ManagedProperty("#{checkoutBean}")
     private CheckoutBean checkoutBean;
     
+    @ManagedProperty("#{loginBean}")
+    private LoginBean loginBean;
+    
     private Integer newItemUPC, newItemQuantity;    
-    private Double newItemWeight;       
+    private Double newItemWeight;   
+    
+    // Discount stuff
+    private Double discountAmount = 0.0, 
+                   discountPercent = 0.0;
 
     public Transaction getCurrentTransaction() {
         return currentTransaction;
@@ -77,16 +85,122 @@ public class TransactionBean {
         this.newItemWeight = newItemWeight;
     }       
 
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }        
+
     public CheckoutBean getCheckoutBean() {
         return checkoutBean;
     }
 
     public void setCheckoutBean(CheckoutBean checkoutBean) {
         this.checkoutBean = checkoutBean;
-    }    
-   
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public Double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(Double discountPercent) {
+        this.discountPercent = discountPercent;
+    }        
+       
     public boolean isTransactionInProgress() {
         return (this.currentTransaction != null);
+    }
+    
+    /**
+     * Adds a discount to the current transaction and redirects/refreshes the
+     * Transaction View page
+     * @return redirect to the Transaction View page.
+     */
+    public String addDiscount() {
+        try {
+            if (loginBean.getCurrentUser().getUserType().compareTo(EUserTypes.Manager) >= 0) {
+                
+                // Create a new discount product
+                Product p = new ProductEntity();                
+                String upc = ""; // the unique one-off UPC we'll be using for the discount product
+                if (discountAmount > 0.0) {
+                    p.setPrice((-1) * discountAmount);
+                    p.setProductName("$" + discountAmount + " DISCOUNT");                    
+                    /* Set the UPC to 19999 plus the discount amount so that we don't
+                     * keep overwriting previous discounts of differing amounts */
+                    upc = ("19999" + (discountAmount*100)).replaceAll("\\.", "");
+                
+                } else if (discountAmount == 0.0 && discountPercent > 0.0) {
+                    p.setPrice(0.0);
+                    p.setProductName("" + (discountPercent*100) + "% DISCOUNT");
+                    /* Set the UPC to 2999 plus the discount percent so that we don't
+                     * keep overwriting previous discounts of differing amounts */
+                    upc = ("29999" + (discountPercent*100)).replaceAll("\\.", "");                
+                } else {
+                    InPageMessage.addInfoMessage("Dollar amount: " + discountAmount);
+                    InPageMessage.addInfoMessage("Percent amount: " + discountPercent);
+                    InPageMessage.addErrorMessage("Please enter a dollar or percent amount.");
+                    return "transaction";
+                }
+                p.setPriceByWeight(false);
+                p.setUPC(Integer.parseInt(upc));
+                p.setProductDescription(p.getProductName());
+                
+                // Create a new discount line item
+                Item i = new ItemEntity();
+                i.setProduct(p); // product is our discount product
+                i.setQuantity(1); // quantity is 1
+                i.setPurchasedWeight(0.0); // purchased weight is 0
+                
+                // Persist the line item
+                ut.begin();
+                em.persist(i);
+                ut.commit();       
+                
+                // add the discount to our transaction
+                this.currentTransaction.addItem(i);                
+                // save the transaction
+                saveTransaction();
+            } else {
+                InPageMessage.addErrorMessage("You are not authorized to add discounts.");
+            }                       
+        } catch (RollbackException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (HeuristicMixedException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (HeuristicRollbackException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (SecurityException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (IllegalStateException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (NotSupportedException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (SystemException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Something went wrong. Please try again.");
+        } catch (NoCurrentSessionException ex) {
+            Logger.getLogger(TransactionBean.class.getName()).log(Level.SEVERE, null, ex);
+            InPageMessage.addErrorMessage("Please log in.");
+            return "index";
+        }        
+        return "transaction";
     }
     
     /**
